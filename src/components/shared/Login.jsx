7521e1ca43913d6ccab10b1aaac0ba9887165fcb
Salman-165324 <salmansadi165324@gmail.com>
2023-06-12 04:37:13 +0600

    Added social and mail-password login system in login page

--- src/components/shared/Login.jsx
+++ src/components/shared/Login.jsx
@@ -3,15 +3,54 @@ import { Link } from "react-router-dom";
 import { AiFillGoogleCircle } from "react-icons/ai";
 import { useForm } from "react-hook-form";
 import PrimaryBtn from "../utils/PrimaryBtn";
+import useAuth from "../../hooks/useAuth";
+import { toast } from "react-hot-toast";
 
 const Login = () => {
+  const { googleSignIn, logInWithPassword } = useAuth();
+
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
-  const onSubmit = (data) => console.log(data);
+  const onSubmit = (data) => {
+    console.log(data);
+    logInWithPassword(data.email, data.password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        toast.success("You have been successfully Logged in", {
+          position: "top-center",
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+        toast.error(error.message, {
+          position: "top-center",
+        });
+      });
+  };
   console.log(errors);
+  const handleGoogleSignIn = () => {
+    googleSignIn()
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        toast.success("You have been successfully signed in", {
+          position: "top-center", // Set the position to top-center
+        });
+      })
+      .catch((error) => {
+        const errorMessage = error.message;
+        toast.error(
+          { errorMessage },
+          {
+            position: "top-center", // Set the position to top-center
+          }
+        );
+      });
+  };
   return (
     <div>
       <div className="primary-container ">
@@ -23,22 +62,10 @@ const Login = () => {
             </div>
             <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
               <div className="card-body">
-                <form>
+                <form onSubmit={handleSubmit(onSubmit)}>
                   <div className="form-control">
                     <label className="label">
-                      <span className="label-text">Name</span>
-                    </label>
-                    <input
-                      className="input input-bordered"
-                      type="text"
-                      placeholder="name"
-                      {...register("name", {})}
-                    />
-                  </div>
-
-                  <div className="form-control">
-                    <label className="label">
-                      <span className="label-text">Email</span>
+                      <span className="label-text">Email*</span>
                     </label>
                     <input
                       className="input input-bordered"
@@ -46,37 +73,36 @@ const Login = () => {
                       placeholder="email"
                       {...register("email", { required: true })}
                     />
-                  </div>
-
-                  <div className="form-control">
-                    <label className="label">
-                      <span className="label-text">PhotoURL</span>
-                    </label>
-                    <input
-                      className="input input-bordered"
-                      type="text"
-                      placeholder="photoURL"
-                      {...register("photoURL", {})}
-                    />
+                    {errors.email?.type === "required" && (
+                      <span className="text-red-500 pl-1 pt-0.5">
+                        This field is required
+                      </span>
+                    )}
                   </div>
                   <div className="form-control">
                     <label className="label">
-                      <span className="label-text">Email</span>
+                      <span className="label-text">Password*</span>
                     </label>
                     <input
                       className="input input-bordered"
-                      type="email"
-                      placeholder="email"
-                      {...register("email", { required: true })}
+                      type="password"
+                      placeholder="password"
+                      {...register("password", {
+                        required: true,
+                      })}
                     />
+                    {errors.password?.type === "required" && (
+                      <span className="text-red-500 pl-1 pt-0.5">
+                        This field is required
+                      </span>
+                    )}
                   </div>
-
                   <div className="form-control mt-6">
                     <PrimaryBtn text={"Login"}></PrimaryBtn>
                   </div>
                 </form>
                 <div className="mt-8">
-                  <button className="btn w-full">
+                  <button onClick={handleGoogleSignIn} className="btn w-full">
                     <AiFillGoogleCircle size={30} />
                     Continue with Google
                   </button>
