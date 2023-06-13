bee9a7499c61d2b0be4c64f9e924038d7f7f6aad
Salman-165324 <salmansadi165324@gmail.com>
2023-06-13 10:52:16 +0600

    Added the axiosSecure hook

---
+++ src/hooks/useAxiosSecure.jsx
@@ -0,0 +1,40 @@
+import axios from "axios";
+import { useEffect } from "react";
+import useAuth from "./useAuth";
+import useAxiosInstance from "./useAxiosInstance";
+import { useNavigate } from "react-router-dom";
+
+const useAxiosSecure = () => {
+  const { user, logout } = useAuth();
+  const navigate = useNavigate();
+  const [axiosInstance] = useAxiosInstance();
+  useEffect(() => {
+    const token = localStorage.getItem("access-token");
+    if (token) {
+      axiosInstance.interceptors.request.use((config) => {
+        config.headers.Authorization = `Bearer ${token}`;
+        return config;
+      });
+    }
+
+    axiosInstance.interceptors.response.use(
+      (response) => response,
+      async (error) => {
+        console.log(error);
+        if (
+          error.response &&
+          (error.response.status === 401 || error.response.status === 403)
+        ) {
+          await logout();
+          navigate("/login");
+          
+        }
+        return Promise.reject(error);
+      }
+    );
+  }, [logout, navigate, axiosInstance]);
+
+  return [axiosInstance];
+};
+
+export default useAxiosSecure;
