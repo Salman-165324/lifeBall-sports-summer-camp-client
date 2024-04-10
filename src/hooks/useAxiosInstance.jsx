2e7af6c16f2c87210db202df2c6b396f1ee4a62d
salman165324 <salmansadi165324@gmail.com>
2024-04-10 07:46:43 +0600

    Added new sever url deployed in cyclic

--- src/hooks/useAxiosInstance.jsx
+++ src/hooks/useAxiosInstance.jsx
@@ -3,11 +3,12 @@ import axios from "axios"
 const useAxiosInstance = () =>{
 
     const axiosInstance = axios.create({
-        baseURL: "https://server-summer-camp-one.vercel.app/",
+        // baseURL: "https://server-summer-camp-one.vercel.app/",
 
         // baseURL:"https://lifeball-summer-camp-server-side.onrender.com"
         // baseURL:"https://lifeball-summercamp-production.up.railway.app"
         // baseURL:"http://localhost:5000", 
+        baseURL:"https://long-seal-rugby-shirt.cyclic.app", 
     })
 
     return [axiosInstance]; 
